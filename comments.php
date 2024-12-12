258b729a83e3cfccb3efd356aa595290e07c6e8a
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 22:44:52 -0800

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,42 +1,23 @@
-<?php
-/**
- * The template for displaying comments
- *
- * This is the template that displays the area of the page that contains both the current comments
- * and the comment form.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Aseel
- * @since 1.0
- * @version 1.0
- */
-
-/*
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
- */
+<?php 
 if ( post_password_required() ) {
 	return;
 }
 ?>
 
 <div id="comments" class="comments-area">
-
-	<?php
-	// You can start editing here -- including this comment!
+	<?php 
 	if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
 			$comments_number = get_comments_number();
-			if ( '1' === $comments_number ) {
+			if ( '1' === $comments_number ) 
+			{
 				/* translators: %s: post title */
 				printf( _x( 'One Reply to &ldquo;%s&rdquo;', 'comments title', 'aseel' ), get_the_title() );
-			} else {
+			} 
+			else 
+			{
 				printf(
-					/* translators: 1: number of comments, 2: post title */
 					_nx(
 						'%1$s Reply to &ldquo;%2$s&rdquo;',
 						'%1$s Replies to &ldquo;%2$s&rdquo;',
@@ -50,7 +31,7 @@ if ( post_password_required() ) {
 			}
 			?>
 		</h2>
-
+	
 		<ol class="comment-list">
 			<?php
 				wp_list_comments( array(
@@ -67,16 +48,13 @@ if ( post_password_required() ) {
 			'next_text' => '<span class="screen-reader-text">' . __( 'Next', 'aseel' ) . '</span>' . aseel_get_svg( array( 'icon' => 'arrow-right' ) ),
 		) );
 
-	endif; // Check for have_comments().
+	endif;
 
-	// If comments are closed and there are comments, let's leave a little note, shall we?
 	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
-
 		<p class="no-comments"><?php _e( 'Comments are closed.', 'aseel' ); ?></p>
 	<?php
 	endif;
 
 	comment_form();
 	?>
-
-</div><!-- #comments -->
+</div>
