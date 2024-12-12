d224347019ce9417f7bd15a95965934220c4107a
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 22:53:11 -0800

    Update template-tags.php

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -1,40 +1,22 @@
-<?php
-/**
- * Custom template tags for this theme
- *
- * Eventually, some of the functionality here could be replaced by core features.
- *
- * @package WordPress
- * @subpackage Aseel
- * @since 1.0
- */
-
+<?php 
 if ( ! function_exists( 'aseel_posted_on' ) ) :
-/**
- * Prints HTML with meta information for the current post-date/time and author.
- */
-function aseel_posted_on() {
-
-	// Get the author name; wrap it in a link.
+function aseel_posted_on() 
+{
 	$byline = sprintf(
 		/* translators: %s: post author */
 		__( 'by %s', 'aseel' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . get_the_author() . '</a></span>'
 	);
-
-	// Finally, let's write all of this to the page.
 	echo '<span class="posted-on">' . aseel_time_link() . '</span><span class="byline"> ' . $byline . '</span>';
 }
 endif;
 
-
 if ( ! function_exists( 'aseel_time_link' ) ) :
-/**
- * Gets a nicely formatted string for the published date.
- */
-function aseel_time_link() {
+function aseel_time_link() 
+{
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) 
+	{
 		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 	}
 
@@ -45,7 +27,6 @@ function aseel_time_link() {
 		get_the_modified_date()
 	);
 
-	// Wrap the time string in a link, and preface it with 'Posted on'.
 	return sprintf(
 		/* translators: %s: post date */
 		__( '<span class="screen-reader-text">Posted on</span> %s', 'aseel' ),
@@ -56,35 +37,26 @@ endif;
 
 
 if ( ! function_exists( 'aseel_entry_footer' ) ) :
-/**
- * Prints HTML with meta information for the categories, tags and comments.
- */
-function aseel_entry_footer() {
-
-	/* translators: used between list items, there is a space after the comma */
+function aseel_entry_footer() 
+{
 	$separate_meta = __( ', ', 'aseel' );
-
-	// Get Categories for posts.
 	$categories_list = get_the_category_list( $separate_meta );
-
-	// Get Tags for posts.
 	$tags_list = get_the_tag_list( '', $separate_meta );
 
-	// We don't want to output .entry-footer if it will be empty, so make sure its not.
-	if ( ( ( aseel_categorized_blog() && $categories_list ) || $tags_list ) || get_edit_post_link() ) {
-
+	if ( ( ( aseel_categorized_blog() && $categories_list ) || $tags_list ) || get_edit_post_link() ) 
+	{
 		echo '<footer class="entry-footer">';
-
-			if ( 'post' === get_post_type() ) {
-				if ( ( $categories_list && aseel_categorized_blog() ) || $tags_list ) {
+			if ( 'post' === get_post_type() ) 
+			{
+				if ( ( $categories_list && aseel_categorized_blog() ) || $tags_list ) 
+				{
 					echo '<span class="cat-tags-links">';
-
-						// Make sure there's more than one category before displaying.
-						if ( $categories_list && aseel_categorized_blog() ) {
+						if ( $categories_list && aseel_categorized_blog() ) 
+						{
 							echo '<span class="cat-links">' . aseel_get_svg( array( 'icon' => 'folder-open' ) ) . '<span class="screen-reader-text">' . __( 'Categories', 'aseel' ) . '</span>' . $categories_list . '</span>';
 						}
-
-						if ( $tags_list ) {
+						if ( $tags_list ) 
+						{
 							echo '<span class="tags-links">' . aseel_get_svg( array( 'icon' => 'hashtag' ) ) . '<span class="screen-reader-text">' . __( 'Tags', 'aseel' ) . '</span>' . $tags_list . '</span>';
 						}
 
