d6aa51859707d98d0ae981aaeb703ae0664d55a0
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 23:04:26 -0800

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -1,54 +1,30 @@
-<?php
-/**
- * The front page template file
- *
- * If the user has selected a static page for their homepage, this is what will
- * appear.
- * Learn more: https://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Aseel
- * @since 1.0
- * @version 1.0
- */
-
-get_header(); ?>
-
+<?php get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-
-		<?php // Show the selected frontpage content.
+		<?php
 		if ( have_posts() ) :
 			while ( have_posts() ) : the_post();
 				get_template_part( 'template-parts/page/content', 'front-page' );
 			endwhile;
-		else : // I'm not sure it's possible to have no posts when this page is shown, but WTH.
+		else :
 			get_template_part( 'template-parts/post/content', 'none' );
-		endif; ?>
+		endif; 
+		?>
 
 		<?php
-		// Get each of our panels and show the post data.
-		if ( 0 !== aseel_panel_count() || is_customize_preview() ) : // If we have pages to show.
-
-			/**
-			 * Filter number of front page sections in Aseel.
-			 *
-			 * @since Aseel 1.0
-			 *
-			 * @param int $num_sections Number of front page sections.
-			 */
+		if ( 0 !== aseel_panel_count() || is_customize_preview() ) :
 			$num_sections = apply_filters( 'aseel_front_page_sections', 4 );
 			global $aseelcounter;
 
-			// Create a setting and control for each of the sections available in the theme.
-			for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
+			for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) 
+			{
 				$aseelcounter = $i;
 				aseel_front_page_section( null, $i );
 			}
 
-	endif; // The if ( 0 !== aseel_panel_count() ) ends here. ?>
-
-	</main><!-- #main -->
-</div><!-- #primary -->
+		endif; 
+		?>
+	</main>
+</div>
 
 <?php get_footer();
