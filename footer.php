01a19f566fce169513472e9edb26bf7615a6630c
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 23:05:03 -0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,19 +1,3 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WordPress
- * @subpackage Aseel
- * @since 1.0
- * @version 1.2
- */
-
-?>
-
 		</div><!-- #content -->
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
@@ -34,13 +18,12 @@
 						?>
 					</nav><!-- .social-navigation -->
 				<?php endif;
-
-				get_template_part( 'template-parts/footer/site', 'info' );
+					get_template_part( 'template-parts/footer/site', 'info' );
 				?>
-			</div><!-- .wrap -->
-		</footer><!-- #colophon -->
-	</div><!-- .site-content-contain -->
-</div><!-- #page -->
+			</div>
+		</footer>
+	</div>
+</div>
 <?php wp_footer(); ?>
 
 </body>
