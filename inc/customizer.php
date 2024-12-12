923df7f3c4df6241ca618d8fca9e758fa1b7c81e
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 22:48:54 -0800

    Update customizer.php

--- inc/customizer.php
+++ inc/customizer.php
@@ -1,18 +1,6 @@
-<?php
-/**
- * Aseel: Customizer
- *
- * @package WordPress
- * @subpackage Aseel
- * @since 1.0
- */
-
-/**
- * Add postMessage support for site title and description for the Theme Customizer.
- *
- * @param WP_Customize_Manager $wp_customize Theme Customizer object.
- */
-function aseel_customize_register( $wp_customize ) {
+<?php 
+function aseel_customize_register( $wp_customize ) 
+{
 	$wp_customize->get_setting( 'blogname' )->transport          = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport   = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport  = 'postMessage';
@@ -26,9 +14,6 @@ function aseel_customize_register( $wp_customize ) {
 		'render_callback' => 'aseel_customize_partial_blogdescription',
 	) );
 
-	/**
-	 * Custom colors.
-	 */
 	$wp_customize->add_setting( 'colorscheme', array(
 		'default'           => 'light',
 		'transport'         => 'postMessage',
@@ -59,9 +44,6 @@ function aseel_customize_register( $wp_customize ) {
 		'priority' => 6,
 	) ) );
 
-	/**
-	 * Theme options.
-	 */
 	$wp_customize->add_section( 'theme_options', array(
 		'title'    => __( 'Theme Options', 'aseel' ),
 		'priority' => 130, // Before Additional CSS.
@@ -85,17 +67,9 @@ function aseel_customize_register( $wp_customize ) {
 		'active_callback' => 'aseel_is_view_with_layout_option',
 	) );
 
-	/**
-	 * Filter number of front page sections in Aseel.
-	 *
-	 * @since Aseel 1.0
-	 *
-	 * @param int $num_sections Number of front page sections.
-	 */
 	$num_sections = apply_filters( 'aseel_front_page_sections', 4 );
-
-	// Create a setting and control for each of the sections available in the theme.
-	for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
+	for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) 
+	{
 		$wp_customize->add_setting( 'panel_' . $i, array(
 			'default'           => false,
 			'sanitize_callback' => 'absint',
@@ -121,12 +95,8 @@ function aseel_customize_register( $wp_customize ) {
 }
 add_action( 'customize_register', 'aseel_customize_register' );
 
-/**
- * Sanitize the page layout options.
- *
- * @param string $input Page layout.
- */
-function aseel_sanitize_page_layout( $input ) {
+function aseel_sanitize_page_layout( $input ) 
+{
 	$valid = array(
 		'one-column' => __( 'One Column', 'aseel' ),
 		'two-column' => __( 'Two Column', 'aseel' ),
@@ -139,11 +109,6 @@ function aseel_sanitize_page_layout( $input ) {
 	return '';
 }
 
-/**
- * Sanitize the colorscheme.
- *
- * @param string $input Color scheme.
- */
 function aseel_sanitize_colorscheme( $input ) {
 	$valid = array( 'light', 'dark', 'custom' );
 
@@ -154,63 +119,37 @@ function aseel_sanitize_colorscheme( $input ) {
 	return 'light';
 }
 
-/**
- * Render the site title for the selective refresh partial.
- *
- * @since Aseel 1.0
- * @see aseel_customize_register()
- *
- * @return void
- */
 function aseel_customize_partial_blogname() {
 	bloginfo( 'name' );
 }
 
-/**
- * Render the site tagline for the selective refresh partial.
- *
- * @since Aseel 1.0
- * @see aseel_customize_register()
- *
- * @return void
- */
 function aseel_customize_partial_blogdescription() {
 	bloginfo( 'description' );
 }
 
-/**
- * Return whether we're previewing the front page and it's a static page.
- */
 function aseel_is_static_front_page() {
 	return ( is_front_page() && ! is_home() );
 }
 
-/**
- * Return whether we're on a view that supports a one or two column layout.
- */
 function aseel_is_view_with_layout_option() {
 	// This option is available on all pages. It's also available on archives when there isn't a sidebar.
 	return ( is_page() || ( is_archive() && ! is_active_sidebar( 'sidebar-1' ) ) );
 }
 
-/**
- * Bind JS handlers to instantly live-preview changes.
- */
-function aseel_customize_preview_js() {
+function aseel_customize_preview_js()
+{
 	wp_enqueue_script( 'aseel-customize-preview', get_theme_file_uri( '/assets/js/customize-preview.js' ), array( 'customize-preview' ), '1.0', true );
 }
 add_action( 'customize_preview_init', 'aseel_customize_preview_js' );
 
-/**
- * Load dynamic logic for the customizer controls area.
- */
-function aseel_panels_js() {
+function aseel_panels_js() 
+{
 	wp_enqueue_script( 'aseel-customize-controls', get_theme_file_uri( '/assets/js/customize-controls.js' ), array(), '1.0', true );
 }
 add_action( 'customize_controls_enqueue_scripts', 'aseel_panels_js' );
 
-/* For infinite scrolling */
-function aseel_posts_infinite_scroll ( WP_Customize_Manager $wp_customize ) {
+function aseel_posts_infinite_scroll ( WP_Customize_Manager $wp_customize ) 
+{
 	$wp_customize->add_section( 'posts_infinite_scroll_section' , array(
 		'title'       => __( 'Infinite Scrolling / Pagination'),
 		'priority'    => 30,
