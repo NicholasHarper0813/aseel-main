21ce3c330bb2c6b9d453d8802fc95d519f27933e
nicholas harper <nicholasharper000@gmail.com>
2024-12-19 18:31:43 -0800

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,15 +1,4 @@
-<?php
-/**
- * Template for displaying search forms in Aseel
- *
- * @package WordPress
- * @subpackage Aseel
- * @since 1.0
- * @version 1.0
- */
-
-?>
-
+<?php ?>
 <?php $unique_id = esc_attr( uniqid( 'search-form-' ) ); ?>
 
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
